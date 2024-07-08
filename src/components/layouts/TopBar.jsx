041d69b7cb7f858330253f54fb0d11c662ebb71f
minjuko <rhalswn0526@gmail.com>
2024-07-08 10:52:32 +0900

    chore: backup

--- src/components/layouts/TopBar.jsx
+++ src/components/layouts/TopBar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import axios from "axios"; // axios 사용
+import axios from "axios";
 
 const TopBars = styled.nav`
   display: flex;
