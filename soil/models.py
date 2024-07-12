599992560c5968ae4f74808ea8de83cd87c5cdc4
suhwan <wnscjfrjsgud@gmail.com>
2024-07-12 17:46:48 +0900

    세션별로 작물,주소 정보 저장

--- soil/models.py
+++ soil/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class crop_data(models.Model):
+    user_id = models.IntegerField()
+    session_id = models.CharField(max_length=255, default=None, null=True, blank=True)
+    crop_name = models.CharField(max_length=255, default=None, null=True, blank=True)
+    address = models.CharField(max_length=255, default=None, null=True, blank=True)
+    detailed_address = models.CharField(max_length=255, default=None, null=True, blank=True)
