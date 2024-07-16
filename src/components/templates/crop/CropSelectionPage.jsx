8c9829f2d1b130dba26604d11d4e7de0b9ad4991
minjuko <rhalswn0526@gmail.com>
2024-07-16 10:57:44 +0900

    chore: clear

--- src/components/templates/crop/CropSelectionPage.jsx
+++ src/components/templates/crop/CropSelectionPage.jsx
@@ -148,21 +148,6 @@ const SaveButton = styled.button`
   }
 `;
 
-const getCSRFToken = () => {
-  let cookieValue = null;
-  if (document.cookie && document.cookie !== '') {
-    const cookies = document.cookie.split(';');
-    for (let i = 0; cookies.length > i; i++) {
-      const cookie = cookies[i].trim();
-      if (cookie.substring(0, 10) === 'csrftoken=') {
-        cookieValue = decodeURIComponent(cookie.substring(10));
-        break;
-      }
-    }
-  }
-  return cookieValue;
-};
-
 const CropSelectionPage = () => {
   const [sessions, setSessions] = useState([]);
   const [error, setError] = useState(null);
